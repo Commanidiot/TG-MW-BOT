f25f217ffc52b840a653970b4c8d9870611d7c50
Commanidiot <86559258+Commanidiot@users.noreply.github.com>
2021-12-28 09:27:33 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,20 +11,7 @@ from .Channel import (
 )
 
 HELP = """
-Basic Commads
-
-/start :- check am Alive
-
-Bot Owner Only
-/broadcast Replay Any Message or Media
-/stats user Status
-/ban_user  click ban_user more info
-/unban_user click unban_user more info
-/banned_users Banned User Details
-/total How Many Files Added In Database
-/logger  Get Logs
-/delete Delete File From Database
-
+CHECK TO SEEEEEEEEE
 """
 
 ABOUT = """
